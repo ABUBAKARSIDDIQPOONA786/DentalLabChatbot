379e977c088f40c73ce1c58021bdeef6a7c1ce05
POONA ABUBAKAR SIDDIQ <132919722+ABUBAKARSIDDIQPOONA786@users.noreply.github.com>
2024-07-21 17:53:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
-import os  # Import os module to handle environment variables
+import os
 
 app = Flask(__name__)
 CORS(app)
